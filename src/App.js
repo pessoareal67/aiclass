522e321a349d0108f488e2ebf8cc5bbc6cc3ade7
pessoareal67 <felipe.racz@saoluis.org>
2026-06-19 19:54:37 -0300

    Set up complete React + GitHub Pages deployment structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to AI Class</h1>
+        <p>Paste your React code here!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
